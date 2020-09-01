ff1846afe1c00a13982b2141ed4036a04152cff4
deatiger <deatiger1492@gmail.com>
2020-09-01 18:16:24 +0900

    Modified how to fetch data via GitHub API

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -21,7 +21,7 @@ export default function Post({ postData }) {
   )
 }
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = await getAllPostIds()
   return {
     paths,
     fallback: false
